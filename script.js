32945417700a64875633bc88a95fb290bb7981df
Barbatinus <mattinkh101@gmail.com>
2020-05-01 16:05:16 +0100

    Added menu animations

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 $('#toggle').click(function() {
   $('.slide-in').toggleClass('show');
 });
+
+$('#main').mouseover(function() {
+    $('.slide-in').toggleClass('show');
+    $('.toggle').toggleClass('open');
+  })
+  $('#main').mouseout(function() {
+    $('.slide-in').toggleClass('show');
+    $('.toggle').toggleClass('open');
+  });
